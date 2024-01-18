223de1cbd6b3c0a7245161a52f938d3879c88480
Fiedler12 <o.lyngholm@gmail.com>
2024-01-18 10:15:42 +0100

    Init

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -18,9 +18,6 @@
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#include "lvgl.h"
-#include "esp_lvgl_port.h"
-#include "esp_lcd_panel_vendor.h"
 
 #include "buffer/ring_buffer.h"
 
@@ -189,28 +186,10 @@ void send_mqtt(char* s, int size) {
   printf("Result of publish: %d\n", x);
 }
 
-void example_lvgl_demo_ui(lv_disp_t *disp)
-{
-    lv_obj_t *scr = lv_disp_get_scr_act(disp);
-    lv_obj_t *label = lv_label_create(scr);
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
-    lv_label_set_text(label, "Hello Espressif, Hello LVGL.");
-    /* Size of the screen (if you use rotation 90 or 270, please set disp->driver->ver_res) */
-    lv_obj_set_width(label, disp->driver->hor_res);
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
-}
-
-static bool notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
-{
-    lv_disp_t * disp = (lv_disp_t *)user_ctx;
-    lvgl_port_flush_ready(disp);
-    return false;
-}
-
-//void setupRGB();
+void setupRGB();
 
 void setup_light_sensor(adc_oneshot_unit_handle_t *handle, adc_oneshot_unit_init_cfg_t *adc_config);
-/*
+
 void turnLightBlue();
 
 void turnLightGreen(); 
@@ -220,7 +199,7 @@ void turnLightRed();
 void turnLightOff();
 
 void ChangeLight(short unsigned int* prev);
-*/
+
 void printBuffer(struct Ring_buffer *buffer);
 
 esp_err_t wake_am(i2c_port_t port, uint8_t am_addr);
@@ -240,62 +219,8 @@ void app_main() {
   esp_err_t err;
   i2c_port = setup_i2c(SDA_PIN, SCL_PIN);
   setup_light_sensor(&handle, &adc_config);
+  setupRGB();
 
-  esp_lcd_panel_io_handle_t io_handle = NULL; 
-  esp_lcd_panel_io_i2c_config_t io_config = {
-        .dev_addr = EXAMPLE_I2C_HW_ADDR,
-        .control_phase_bytes = 1,               // According to SSD1306 datasheet
-        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,   // According to SSD1306 datasheet
-        .lcd_param_bits = EXAMPLE_LCD_CMD_BITS,
-        .dc_bit_offset = 6,
-  };
-  
-  ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c((esp_lcd_i2c_bus_handle_t)i2c_port, &io_config, &io_handle));
-
-  ESP_LOGI(TAG, "Install SSD1306 panel driver");
-    esp_lcd_panel_handle_t panel_handle = NULL;
-    esp_lcd_panel_dev_config_t panel_config = {
-        .bits_per_pixel = 1,
-        .reset_gpio_num = EXAMPLE_PIN_NUM_RST,
-    };
-
-  ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(io_handle, &panel_config, &panel_handle));
-
-  ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
-  ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
-  ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
-  //setupRGB();
-
-  const lvgl_port_cfg_t lvgl_cfg = ESP_LVGL_PORT_INIT_CONFIG();
-  lvgl_port_init(&lvgl_cfg);
-
-
-  const lvgl_port_display_cfg_t disp_cfg = {
-      .io_handle = io_handle,
-      .panel_handle = panel_handle,
-      .buffer_size = EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES,
-      .double_buffer = true,
-      .hres = EXAMPLE_LCD_H_RES,
-      .vres = EXAMPLE_LCD_V_RES,
-      .monochrome = true,
-      .rotation = {
-          .swap_xy = false,
-          .mirror_x = false,
-          .mirror_y = false,
-      }
-  };
-
-  lv_disp_t * disp = lvgl_port_add_disp(&disp_cfg);
-
-  const esp_lcd_panel_io_callbacks_t cbs = {
-    .on_color_trans_done = notify_lvgl_flush_ready,
-  };
-
-  esp_lcd_panel_io_register_event_callbacks(io_handle, &cbs, disp);
-
-  lv_disp_set_rotation(disp, LV_DISP_ROT_NONE);
-
-  example_lvgl_demo_ui(disp);
 
   char packet[50];
   int ctr = 0;
@@ -418,7 +343,7 @@ esp_err_t read_am_temp(i2c_port_t port, uint8_t am_addr, uint16_t* temp_reading)
   *temp_reading = (((uint16_t) rbuf[2]) << 8) | ((uint16_t) rbuf[3]);
   return ESP_OK; 
 }
-/*
+
 void turnLightGreen() {
   gpio_set_level(GREEN_PIN, false);
   gpio_set_level(RED_PIN, true);
@@ -464,7 +389,7 @@ void setupRGB() {
   gpio_set_direction(BLUE_PIN, GPIO_MODE_OUTPUT);
   gpio_set_direction(GREEN_PIN, GPIO_MODE_OUTPUT);
 }
-*/
+
 void setup_light_sensor(adc_oneshot_unit_handle_t *handle, adc_oneshot_unit_init_cfg_t *adc_config) {
   esp_err_t err;
 
